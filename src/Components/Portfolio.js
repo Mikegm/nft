e0ad780b80b2804099f381597e2fbb6c1a89de61
Miguel Garcia <miguelgarcia@MacBook-Pro.local>
2021-11-08 21:59:31 -0600

    modal

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -18,9 +18,9 @@ class Portfolio extends Component {
         <div className="container">
         <div className="  roadmap__item roadmap__item--completed">
               <h2>Roadmap</h2>   
-            </div>
-          <div className="row">
-            <div className="col-12 col-md-6 ">
+           
+          <div className="row m-auto">
+            <div className="col-12 col-sm-6 col-md-6 ">
             <p>
               Project launch
 
@@ -29,13 +29,13 @@ Round 1 (presale) - Whitelist early access for first 100 OG Chicks holders.
 Round 2 - the main sales launch for everyone.
               </p>
             </div>
-            <div className="col-12 col-md-6">
-            <img src="/images/chick7.jpeg" className="d-block w-100" alt="..." />
+            <div className="col-12 col-sm-6  col-md-6">
+            <img src="/images/chick7.jpeg" className="d-block w-100 img-fluid" alt="..." />
             </div>
-            <div className="col-12 col-md-6 mt-5">
-            <img src="/images/chick7.jpeg" className="d-block w-100" alt="..." />
+            <div className="col-12 col-sm-6 col-md-6 mt-5">
+            <img src="/images/chick7.jpeg" className="d-block w-100 img-fluid" alt="..." />
             </div>
-            <div className="col-12 col-md-6  mt-5">
+            <div className="col-12 col-sm-6 col-md-6  mt-5">
             <p>
               Project launch
 
@@ -44,7 +44,7 @@ Round 1 (presale) - Whitelist early access for first 100 OG Chicks holders.
 Round 2 - the main sales launch for everyone.
               </p>
             </div>
-            <div className="col-12 col-md-6 mt-5">
+            <div className="col-12 col-sm-6 col-md-6 mt-5">
             <p>
               Project launch
 
@@ -53,12 +53,41 @@ Round 1 (presale) - Whitelist early access for first 100 OG Chicks holders.
 Round 2 - the main sales launch for everyone.
               </p>
             </div>
-            <div className="col-12 col-md-6 mt-5">
-            <img src="/images/chick7.jpeg" className="d-block w-100" alt="..." />
+            <div className="col-12 col-sm-6 col-md-6 mt-5">
+            <img src="/images/chick7.jpeg" className="d-block w-100 img-fluid" alt="..." />
             </div>
+          </div>
+          <a href="#" className="button mt-5 btn-modal" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                      <i className="fa fa-Discord"></i>Modal
+                    </a>
           </div>
           </div>
         </Fade>
+
+
+  {/* Modal */}
+<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">Congratulation</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+        <p className="token">  This is your Token <span>#C01</span></p>
+       
+        <div className="avatar">
+        <img src="/images/chick7.jpeg" className="d-block w-100 img-fluid" alt="..." />
+        </div>
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="button btn-cancel" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="button">View</button>
+      </div>
+    </div>
+  </div>
+</div>
+
       </section>
     );
   }
