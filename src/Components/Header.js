25ef8993618be930ee7f49f0eeea165c901551a0
Miguel Garcia <miguelgarcia@MacBook-Pro.local>
2021-11-09 19:41:43 -0600

    form en sidebar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -24,7 +24,25 @@ class Header extends Component {
   </div>
   <div className="offcanvas-body">
     <div>
-     Lorem ipsum 
+     <form>
+     <div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Email address</label>
+  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+</div>
+<div className="mb-3">
+  <label for="name" className="form-label">Email address</label>
+  <input type="text" className="form-control" id="name" placeholder="Name"/>
+</div>
+<div className="mb-3">
+  <label for="exampleFormControlTextarea1" className="form-label"> textarea</label>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+<div className="mb-3">
+<a href="#" className="button">
+                    Send
+                    </a>
+</div>
+     </form>
     </div>
     
   </div>
